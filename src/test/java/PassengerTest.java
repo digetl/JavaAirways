4d4994eb36dbcbfb8afc35049bdfba6a833259d9
digetl <dave@digetl.co.uk>
2020-06-19 11:34:48 +0100

    Test passenger hasName - failed

--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,4 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PassengerTest {
 
+    private Passenger passenger;
+
+    @Before
+    public void before() {
+        passenger = new Passenger("Dave Campbell");
+    }
+
+    @Test
+    public void hasName() {
+    assertEquals("Dave Campbell", passenger.getName());
+    }
 
 }
