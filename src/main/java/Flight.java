e8d270916e33ea12966a7b2af33531c90b79417f
digetl <dave@digetl.co.uk>
2020-06-19 13:30:17 +0100

    Flight has departure airport - passed

--- src/main/java/Flight.java
+++ src/main/java/Flight.java
@@ -1,11 +1,17 @@
 public class Flight {
     private String destination;
+    private String departureAirport;
 
-    public Flight(String destination){
+    public Flight(String destination, String departureAirport){
         this.destination = destination;
+        this.departureAirport = departureAirport;
     }
 
     public String getDestination() {
         return destination;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
     }
 }
