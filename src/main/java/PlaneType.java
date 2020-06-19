2c846b7868b1b62ea16bce7b120d401839e85219
digetl <dave@digetl.co.uk>
2020-06-19 13:09:38 +0100

    Added 3 PlaneTypes

--- src/main/java/PlaneType.java
+++ src/main/java/PlaneType.java
@@ -1,2 +1,14 @@
 public enum PlaneType {
+    CESSNA(100, 400),
+    CONAIR(400,1600),
+    PIPER(150,600);
+
+
+    private final int capacity;
+    private final int weightLimit;
+
+    PlaneType(int capacity, int weightLimit) {
+        this.capacity = capacity;
+        this.weightLimit = weightLimit;
+    }
 }
