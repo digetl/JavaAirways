a0144fda5e09462b17db94469590e4850a7a65b3
digetl <dave@digetl.co.uk>
2020-06-19 13:19:23 +0100

    Flight has destination - failed

--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -1,2 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class FlightTest {
+
+   private Flight flight1;
+   private Flight flight2;
+   private Flight flight3;
+
+   @Before
+    public void before(){
+       flight1 = new Flight("GAT", "JA1445","13:18","EDI");
+   }
+
+   @Test
+    public void hasDestination() {
+       assertEquals("GAT", flight1.getDestination());
+   }
+
 }
