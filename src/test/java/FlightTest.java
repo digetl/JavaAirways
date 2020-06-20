e938039487f09682ff5b9a05adcfca15dfaf01a8
digetl <dave@digetl.co.uk>
2020-06-20 11:38:16 +0100

    hasAPlaneAssigned - passed

--- src/test/java/FlightTest.java
+++ src/test/java/FlightTest.java
@@ -10,11 +10,15 @@ public class FlightTest {
    private Flight flight2;
    private Flight flight3;
    private Plane plane1;
+   private Passenger passenger1;
+   private Passenger passenger2;
 
    @Before
     public void before(){
+      passenger1 = new Passenger("Dave Campbell", 0);
+      passenger2 = new Passenger("Kirsty Campbell", 2);
       plane1 = new Plane("CS123", PlaneType.CESSNA);
-       flight1 = new Flight("GAT", "EDI","JA1445", "13:18", plane1);
+      flight1 = new Flight("GAT", "EDI","JA1445", "13:18", plane1);
    }
 
    @Test
@@ -46,5 +50,10 @@ public class FlightTest {
    public void hasAPlaneAssigned(){
       assertEquals(plane1, flight1.getPlane());
    }
+
+//   public void getNumberOfFreeSeats() {
+//      assertEquals(flight1);
+//   }
+
 
 }
